4190c1f39e2c5412e4319f7624bdaf3c9bfd59a1
Levy <zly.george@163.com>
2016-03-15 14:24:34 +0800

    abandon the site. Redirect to personal blog

--- http_server.py
+++
@@ -1,7 +0,0 @@
-PORT = 8000
-
-import SimpleHTTPServer,SocketServer
-Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
-httpd = SocketServer.TCPServer(("", PORT), Handler)
-print "serving at port", PORT
-httpd.serve_forever()
